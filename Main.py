2d574998417e5bd5092b284f510d27eedae1f14c
Talon <asd@asd.com>
2018-02-04 03:07:49 +0100

    .

--- Main.py
+++ Main.py
@@ -1,6 +1,16 @@
 import numpy as np;
+import math as math
 import matplotlib as mm;
 
-a = np.arange(15).reshape(3, 5);
-print(a);
-print(a);
+def javaDifferences ():
+    num = 1;
+    # can't use num++, ** for Math.pow(),
+    num += 1; # 2
+    num **= 3; # 8
+    num = math.sqrt(num+1);
+    print(num);
+    return;
+
+
+# a = np.arange(15).reshape(3, 5);
+javaDifferences();
