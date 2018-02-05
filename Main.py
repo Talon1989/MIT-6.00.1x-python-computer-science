11818d0b37537db7f2bbd0670d6e2e249012c1c8
Talon <asd@asd.com>
2018-02-05 05:32:59 +0100

    added problems.

--- Main.py
+++ Main.py
@@ -7,53 +7,62 @@ import One as one;
 def javaDifferences():
     num = 1;
     # can't use num++, ** for Math.pow(),
-    num += 1; # 2
-    num **= 3; # 8
-    num = math.sqrt(num+1);
+    num += 1;  # 2
+    num **= 3;  # 8
+    num = math.sqrt(num + 1);
     print(num);
 
     # PRINT
-    print("hello"+"world");
-    print("hello", "world"); # ',' means +' '+
+    print("hello" + "world");
+    print("hello", "world");  # ',' means +' '+
 
     # ARRAY
     potato = "potato";
-    potato[:2]; # 'po'
-    potato[1:3]; # 'ot'
-    potato[::2]; # skips 2 start from 0 'ptt'
-    potato[1::2]; # skips 2 start from 1 'oao'
-    print(potato[1:]) # 'otato'
+    potato[:2];  # 'po'
+    potato[1:3];  # 'ot'
+    potato[::2];  # skips 2 start from 0 'ptt'
+    potato[1::2];  # skips 2 start from 1 'oao'
+    print(potato[1:])  # 'otato'
 
     # CASTING
-    x = 1; # int
-    x_str = str(x); # equivalent of (String) x
+    x = 1;  # int
+    x_str = str(x);  # equivalent of (String) x
     print(type(x_str));
 
     # INPUT
-    try:
-        my_variable = input("Type anything..."); # input = new Scanner(System.in).nextLine();
-        my_int_variable = int(input("Type a number")); # casts into an int, Integer.parseInt();
-        print(my_int_variable);
-    except ValueError:
-        print("Count not convert data to integer");
+    while True:
+        try:
+            my_variable = input("Type anything...");  # input = new Scanner(System.in).nextLine();
+            my_int_variable = int(input("Type a number"));  # casts into an int, Integer.parseInt();
+            print(my_int_variable);
+            break;
+        except ValueError:
+            print("Count not convert data to integer");
+
+    # FOR LOOP
+    for i in range(5): # 0 1 2 3 4
+        print(i);
+    print();
+    for i in range(0, 6, 2): # from 0 to 5, increments by 2
+        print(i);
 
 
 
 def findNum(num: int, arr, startNum: int, endNum: int):
-    mid = (startNum+endNum) // 2;
-    print("start: "+str(startNum)+" / end: "+str(endNum));
-    print("mid: "+str(mid)+" / number at mid: "+str(arr[mid])+" / number to find: "+str(num));
+    mid = (startNum + endNum) // 2;
+    print("start: " + str(startNum) + " / end: " + str(endNum));
+    print("mid: " + str(mid) + " / number at mid: " + str(arr[mid]) + " / number to find: " + str(num));
     if num == arr[mid]:
         print("should yes")
         return True;
-    elif startNum == endNum or startNum == endNum-1 or startNum == endNum+1:
+    elif startNum == endNum or startNum == endNum - 1 or startNum == endNum + 1:
         print("should exit");
         return False;
     elif num < arr[mid]:
-        print("LESS : current "+str(arr[mid])+" / want: "+str(num));
+        print("LESS : current " + str(arr[mid]) + " / want: " + str(num));
         findNum(num, arr, startNum, mid);
     elif num > arr[mid]:
-        print("MORE : current "+str(arr[mid])+" / want: "+str(num));
+        print("MORE : current " + str(arr[mid]) + " / want: " + str(num));
         findNum(num, arr, mid, endNum);
     print("noff");
     return;
--- One.py
+++ One.py
@@ -4,5 +4,45 @@ def potato():
 
 def swap(one, two):
     print("value of one: "+one+" \\ value of two: "+two);
-    temp = one; one = two; two = temp;
+    temp = one; one = two;
+    two = temp;
     print("value of one: "+one+" \\ value of two: "+two);
+
+
+def countWords(s):
+    counter = 0;
+    prev1 = '';
+    prev2 = '';
+    for letter in s:
+        if letter == 'b' and prev1 == 'o' and prev2 == 'b':
+            counter += 1;
+        prev2 = prev1;
+        prev1 = letter;
+    print("Number of times bob occurs is: " + str(counter));
+
+
+def findLongestStringAlphOrder(s):
+    count = 0; max_count = 0; last_position = 0;
+    for i in range(len(s) - 1):
+        if s[i] <= s[i+1]:
+            count += 1;
+            if count > max_count:
+                max_count = count;
+                last_position = i+1;
+        else:
+            count = 0;
+    first_position = last_position-max_count;
+    print(s[first_position:last_position+1]);
+
+
+def findPerfectSquare(number):
+    ans = 0;
+    while ans**2 < abs(number):
+        ans += 1;
+    if ans**2 == abs(number):
+        print(str(number), "is a perfect square");
+    else:
+        print(str(number), "is not a perfect square");
+
+
+findPerfectSquare(25);
