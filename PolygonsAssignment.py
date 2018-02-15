ae5bd47885efbf966428920f6224997765115cf2
Talon <asd@asd.com>
2018-02-15 13:33:45 +0100

    fixed.

--- PolygonsAssignment.py
+++ PolygonsAssignment.py
@@ -7,6 +7,6 @@ def polysum(n, s):
     :param s: length of each side
     :return: sum of the area and the square of the perimeter of polygon with given sides, .4 digit precision
     '''
-    area = (0.25 * n * (s ** 2)) / cmath.tan(cmath.pi / n)
+    area = (0.25 * n * s ** 2) / cmath.tan(cmath.pi / n)
     perimeter = n * s
-    return round((cmath.sqrt(perimeter) + area).real, 4)
+    return round((perimeter ** 2 + area).real, 4)
